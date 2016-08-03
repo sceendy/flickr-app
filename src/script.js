bc3e2b81fda01d722de0a6de06df56610c44cc9b
Cindy Juarez <sceendy@gmail.com>
2016-08-02 23:50:43 -0500

    added babel for safari

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 /* NOTE for javascript
+    Browser compatibility on current release, current release-1 versions of browsers
     Page load times and performance
     Sorting and or filter options available to user
 */
@@ -7,17 +8,21 @@
 const apiKey = 'bd9987dd291178d6ec1886920d9cd7c8';
 const userId = '24662369@N07';
 
-// cool, let's use it now
+// cool, let's use those now
 window.onload = () => {
+  // things that won't change
   const imageList = document.getElementById('items');
+  const itemsDesc = document.getElementsByClassName('items__description')[0];
   const previousBtn = document.getElementsByClassName('pagination__btn--left')[0];
   const nextBtn = document.getElementsByClassName('pagination__btn--right')[0];
-  const itemsDesc = document.getElementsByClassName('items__description')[0];
+  const sortForm = document.getElementById('sortBy');
+  const loadingImage = "<img src='https://33.media.tumblr.com/0ba1ba128951dff1eea53c9a35d8fe0d/tumblr_n4ux8nbU3m1sjwwzso1_500.gif' class='block--center'>";
 
-  let loadingImage = "<img src='https://33.media.tumblr.com/0ba1ba128951dff1eea53c9a35d8fe0d/tumblr_n4ux8nbU3m1sjwwzso1_500.gif'>";
+  // setting the api url to grab photo view count to use for sorting function
+  // and only requesting 15 photos per page.
   let apiUrl = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos' +
                 '&api_key=' + apiKey +  '&user_id=' + userId + '&format=json&nojsoncallback=1' +
-                '&extras=views%2C+url_q&per_page=15';
+                '&extras=views&per_page=12';
   let errorMsg = loadingImage + "<p class='text--center'>Some error occurred.</p>";
   let httpRequest = new XMLHttpRequest();
   let totalPages;
@@ -28,8 +33,24 @@ window.onload = () => {
   // some sort of cute placeholder
   imageList.innerHTML = loadingImage;
 
+  // event listeners
+  sortForm.addEventListener('change', (e) => {
+    sortBy(e.target.value);
+  });
+
+  // switched to eventListeners for Babel/Safari9
+  previousBtn.addEventListener('click', () => {
+    previousPage();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    nextPage();
+  });
+
   // function to make the GET call to the flickr API
-  getPhotos = (page) => {
+  let getPhotos = (page) => {
+    sortForm.value = 'newest';
+
     if (page === 1) {
       previousBtn.style.display = 'none';
     } else {
@@ -41,38 +62,47 @@ window.onload = () => {
     httpRequest.onload = () => {
       if (httpRequest.status >= 200 && httpRequest.status < 400) {
         clear();
-        data = JSON.parse(httpRequest.responseText).photos;
+        let data = JSON.parse(httpRequest.responseText).photos;
         photos = data.photo;
         totalPages = data.pages;
         currentPage = data.page;
 
+        itemsDesc.innerHTML = 'Page ' + currentPage + ' of ' + totalPages;
+
         // for every photo in the returned array,
         for (let p in photos) {
+          // generate photo link (could also request flicker to send url_q value but ehh more data)
+          let imageUrl = 'https://farm' + photos[p].farm + '.staticflickr.com/' + photos[p].server +
+                          '/' + photos[p].id + '_' + photos[p].secret + '.jpg';
+          photos[p].url = imageUrl;
+
           // let's send it as an argument to create a list item per photo
           createListItem(photos[p]);
         }
 
       } else {
         // render error message for user
-        itemsDesc.innerHTML = errroMsg;
+        itemsDesc.innerHTML = errorMsg;
       }
     };
 
     httpRequest.onerror = () => {
       // render error message for user
-      itemsDesc.innerHTML = errroMsg;
+      itemsDesc.innerHTML = errorMsg;
     };
 
+    // let's do this
     httpRequest.send();
   };
 
-  // initial call to flickr API
+  // initial call to flickr API, get photos from first page
   getPhotos(1);
 
   // for each photo from the array, a list item element is created
-  createListItem = (i) => {
+  let createListItem = (i) => {
     let item = document.createElement('li');
-    item.innerHTML = "<img src='" + i.url_q +  "' alt='" + i.title + "'/>";
+    item.classList.add('img__container');
+    item.innerHTML = "<img src='" + i.url +  "' alt='" + i.title + "'/>";
 
     // and then, that list item is attached to
     // the unordered list to be rendered HTML file
@@ -80,23 +110,28 @@ window.onload = () => {
   };
 
   // use current page to determine what the next page is
-  nextPage = () => {
+  let nextPage = () => {
     // if the current page matches the numbers of pages, stop.
     if (currentPage !== totalPages) {
       getPhotos(currentPage + 1);
     } else {
       nextBtn.style.display = 'none';
     }
+
+    window.scrollTo(0, 0);
   };
 
   // use current page to determine what the previous page was
-  previousPage = () => {
+  let previousPage = () => {
     if (currentPage !== 1) {
       getPhotos(currentPage - 1);
     }
+
+    window.scrollTo(0, 0);
   };
 
-  byViews = (a, b) => {
+  let byViews = (a, b) => {
+    // check photo view count to compare and resort the photos
     let first = Number(a.views);
     let next = Number(b.views);
     if (sortedBy == 'mostViews') {
@@ -106,27 +141,24 @@ window.onload = () => {
     }
   };
 
-  document.getElementById('sortBy').addEventListener('change', (e) => {
-    sortBy(e.target.value);
-  });
-
-  // sorter method
-  sortBy = (arg) => {
+  // sorter function
+  let sortBy = (arg) => {
     sortedBy = arg;
 
     // sort images by most views
     photos.sort(byViews);
 
-    // then clear imageList for sorted results
+    // then clear previous image order for newly sorted image order
     clear();
 
-    // run list item creation method to render sorted results
+    // run list item creation function to render sorted results
     for (let p in photos) {
       createListItem(photos[p]);
     }
   };
 
-  clear = () => {
+  // function to clear values
+  let clear = () => {
     imageList.innerHTML = '';
     itemsDesc.innerHTML = '';
   };
